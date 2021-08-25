f04c52041fa10adfa0e412ff2005d2b597409288
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-08-25 14:11:37 +0800

    [UITalk] 08. 自定义控件SplashView

---
+++ canvas_api/src/main/java/com/jesen/canvas_api/splash/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.jesen.canvas_api.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jesen.canvas_api.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
