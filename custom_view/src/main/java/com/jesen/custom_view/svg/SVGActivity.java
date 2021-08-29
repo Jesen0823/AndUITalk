3550d5f8188f2c978fd70d3508b579a707bf8472
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-08-29 23:13:21 +0800

    [UITalk] 22. add custom view of svg

---
+++ custom_view/src/main/java/com/jesen/custom_view/svg/SVGActivity.java
@@ -0,0 +1,16 @@
+package com.jesen.custom_view.svg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jesen.custom_view.R;
+
+public class SVGActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_svgactivity);
+    }
+}
